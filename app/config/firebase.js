156643ae073ab9cd23b624f92167261e17d4b0a9
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2024-05-08 16:23:36 +0000

    updated firebase confi

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYWvq_hrbKlPIh0PFipUDRgq0VXU8xMwM",
-  authDomain: "reecoplast-46ffa.firebaseapp.com",
-  projectId: "reecoplast-46ffa",
-  storageBucket: "reecoplast-46ffa.appspot.com",
-  messagingSenderId: "309032738238",
-  appId: "1:309032738238:web:35894f6a10818d67cf2def"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
